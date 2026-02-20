c4e86e089c5f4e7488beb17e8eec3e463e6a7b7c
ephtale <haruaozaki@outlook.com>
2026-02-20 11:54:23 +0800

    Fix style

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -14,26 +14,27 @@ import seedu.address.model.person.Remark;
  */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
 
-	/**
-	 * Parses the given {@code String} of arguments in the context of the RemarkCommand
-	 * and returns a RemarkCommand object for execution.
-	 *
-	 * @throws ParseException if the user input does not conform the expected format
-	 */
-	@Override
-	public RemarkCommand parse(String args) throws ParseException {
-	    requireNonNull(args);
-	    ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemarkCommand
+     * and returns a RemarkCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
 
-	    Index index;
-	    try {
-		    index = ParserUtil.parseIndex(argMultimap.getPreamble());
-		} catch (ParseException pe) {
-		    throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    RemarkCommand.MESSAGE_USAGE), pe);
-		}
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), pe);
+        }
 
-	    String remarkText = argMultimap.getValue(PREFIX_REMARK).orElse("");
-	    return new RemarkCommand(index, new Remark(remarkText));
-	}
+        String remarkText = argMultimap.getValue(PREFIX_REMARK).orElse("");
+        return new RemarkCommand(index, new Remark(remarkText));
+    }
 }
+
